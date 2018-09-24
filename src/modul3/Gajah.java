d26560adf28775f0611685d9b177f32454ef6aa8
Fariz <Fariz@LAPTOP-JQ2AQ0I3>
2018-09-24 13:17:30 +0700

    Modul3

--- src/modul3/Gajah.java
+++ src/modul3/Gajah.java
@@ -10,5 +10,20 @@ package modul3;
  * @author Fariz
  */
 public class Gajah {
+     public static void testClassMethod() {
+        System.out.println("The Class Method in Hewan ... ");
+    }
+
+    public void testInstanceMethod() {
+        System.out.println("The Instance Method in Gajah ... ");
+    }
+
+    public static void main(String[] args) {
+        Gajah myGajah = new Gajah();
+        Gajah myHewan = myGajah;
+        Hewan.testClassMethod();
+        myHewan.testInstanceMethod();
+    }
     
 }
+
--- src/modul3/Hewan.java
+++ src/modul3/Hewan.java
@@ -10,5 +10,12 @@ package modul3;
  * @author Fariz
  */
 public class Hewan {
+     public static void testClassMethod(){
+        System.out.println("The Class Method in Hewan...");
+    }
+    public void testInstanceMethod(){
+        System.out.println("The Instance Method in Hewan...");
+    }
     
 }
+
