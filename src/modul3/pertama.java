f682c461c608fc84804f0d7c3dc11456742ecbf3
Fariz <Fariz@LAPTOP-JQ2AQ0I3>
2018-09-24 13:01:40 +0700

    Modul3

--- src/modul3/pertama.java
+++ src/modul3/pertama.java
@@ -10,5 +10,13 @@ package modul3;
  * @author Fariz
  */
 public class pertama {
-    
-}
+    private int a =10;
+    protected void terprotek(){
+        System.out.println("Method ini hanya untuk anaknya.");
+    }
+    public void info(){
+        System.out.println("a = "+a);
+        System.out.println("Dipanggil pada = "+this.getClass().getName());
+       
+    }
+    }
